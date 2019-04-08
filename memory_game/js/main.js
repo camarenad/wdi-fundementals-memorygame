a8d64699bdad99fdadcbd0d9be4d80a8e42b0b7e
Daniel Camarena <dcamarena0229@gmail.com>
2019-04-08 16:17:18 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -2,7 +2,22 @@
 
 // initializing the card variables
 
-var cardOne = 'Queen';
-var cardTwo = 'Queen';
-var cardThree = 'King';
-var cardFour = 'King';
+var cards = ['queen','queen','king','king'];
+var cardsInPlay = [];
+var cardOne = cards[0];
+var cardTwo = cards[2];
+
+cardsInPlay.push(cardOne,cardTwo);
+// cardsInPlay.push(cardTwo);
+
+console.log("user flipped" + " " + cardsInPlay[0]);
+console.log("user flipped" + " " + cardsInPlay[1]);
+
+
+if (cardsInPlay.length === 2) {
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+        alert('You found a match!');
+    } else {
+        alert('Sorry try again.')
+    }
+}
